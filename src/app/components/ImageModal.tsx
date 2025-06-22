3c2e97e0f33229992dfb494f87970cb393484d44
Jafet Montilla <jafetj@hotmail.com>
2025-06-22 00:06:02 -0400

    Customize swiper navigation styles in ImageModal and TechnologiesModal; update button text color for consistency

--- src/app/components/ImageModal.tsx
+++ src/app/components/ImageModal.tsx
@@ -49,7 +49,7 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onRequestClose, imageUr
           navigation
           pagination={{ clickable: true }}
           initialSlide={initialSlide}
-          className="w-full h-full"
+          className="w-full h-full image-carousel"
         >
           {imageUrls.map((url, index) => (
             <SwiperSlide key={index}>
@@ -68,7 +68,7 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onRequestClose, imageUr
         </Swiper>
         <button
           onClick={onRequestClose}
-          className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full p-2 w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-gray-900 transition-all duration-300 z-10"
+          className="absolute top-4 right-4 text-gray-400 bg-black bg-opacity-50 rounded-full p-2 w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-gray-900 transition-all duration-300 z-10"
         >
           X
         </button>
--- src/app/components/TechnologiesModal.tsx
+++ src/app/components/TechnologiesModal.tsx
@@ -46,7 +46,7 @@ const TechnologiesModal: React.FC<TechnologiesModalProps> = ({ isOpen, onRequest
       </ul>
       <button
         onClick={onRequestClose}
-        className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full p-2 w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-gray-900 transition-all duration-300"
+        className="absolute top-4 right-4 text-gray-400 bg-black bg-opacity-50 rounded-full p-2 w-10 h-10 flex items-center justify-center cursor-pointer hover:bg-gray-900 transition-all duration-300"
       >
         X
       </button>
